ecf0eeb898b3100e1228b296041647337d9e94e4
Seulgi-Yoo <ysg213@naver.com>
2023-03-02 23:07:55 +0900

    주석 수정

--- power.js
+++ power.js
@@ -4,7 +4,7 @@ function power(base, exponent) {
   if (exponent === 0) return 1;
 
   // exponent가 짝수인 경우 base^(exponent/2)를 구한 뒤 제곱(ex: 2^10 = 2^5 * 2^5)한 값 % 94906249 리턴
-  // exponent가 홀수인 경우 base^((exponent - 1)/2)를 구한 뒤 제곱(ex: 2^11 = 2^5 * 2^5 * 2)한 값 % 94906249 리턴
+  // exponent가 홀수인 경우 base^((exponent - 1)/2)를 구한 뒤 제곱 * base(ex: 2^11 = 2^5 * 2^5 * 2)한 값 % 94906249 리턴
   const half = power(base, ~~(exponent / 2)); //? ~~(exponent / 2) => (exponent / 2)와 (exponent - 1) / 2를 동시에 계산하는 방법
   const result = (half * half) % 94906249;
   if (exponent % 2 === 1) {
